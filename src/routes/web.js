005f67e611ee91c02f98a82539f1ce9c1328162f
caytaonohoa1609 <164864433+caytaonohoa1609@users.noreply.github.com>
2024-09-15 17:27:27 +0700

    Create New Users - CRUD với Node.JS (Create)

--- src/routes/web.js
+++ src/routes/web.js
@@ -13,7 +13,9 @@ const initWebRoutes = (app) => {
     //path, handler
     router.get("/", homeController.handleHelloWord);
     router.get("/user", homeController.handleUserPage);
-
+    // method get tượng trưng cho việc lấy data từ phía server
+    router.post("/users/create-user", homeController.handleCreateNewUser);
+    // method post tượng trưng cho việc đẩy data lên phía server (create)
     return app.use("/", router);
 
 }
